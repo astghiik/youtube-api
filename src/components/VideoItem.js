c12f65ee8b6a335a8af5f54cc6e0b31b0b6a5252
Astghik Khachatryan <astghikhachatrian@gmail.com>
2019-10-22 01:15:24 +0400

    layouts

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
 
 const VideoItem = props => {
     const { video } = props;
-    console.log(video.snippet.thumbnails.medium.url)
 
     return (
-        <div>
-            <img src={video.snippet.thumbnails.medium.url} alt="as" />
+        <div style={{cursor: "pointer"}}>
+            <img src={video.snippet.thumbnails.medium.url} alt="thumbnail" />
+            <Typography>
+                {video.snippet.title}
+                {video.id.channelId ? <img src="https://image.flaticon.com/icons/png/512/49/49672.png" alt="channel logo" height="19" width="19" style={{marginLeft: "12px"}}/> : null }
+            </Typography>
         </div>
     )
 }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,15 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import VideoItem from './VideoItem';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
 
 const VideoList = props => {
     const { videos } = props;
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
 
-    const videoItems = videos.map(v => <VideoItem key={v.id.channelId} video={v}/>);
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = event => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+        
+
+    const videoItems = videos.map(v => {
+        return (
+            // <Grid item xs key={v.id.videoId ? v.id.videoId : v.id.channelId}>
+            //     <Paper style={{height: "100%"}}>
+            //         <VideoItem video={v}/>
+            //         {v.snippet.title}
+            //     </Paper>
+            // </Grid>
+
+            <TableCell key={v.id.videoId ? v.id.videoId : v.id.channelId} align="center">
+                <VideoItem video={v}/>
+                {/* <Typography>{v.snippet.title}</Typography> */}
+            </TableCell>
+        )
+    });
     
     return (
-        <div>
-            {videoItems}
-        </div>
+        // <div>
+        //     <Grid container justify="center" spacing={3}>
+        //         {videoItems}
+        //     </Grid>
+        // </div>
+        videos.length ? (
+        <Paper>
+            <div>
+                <Table
+                    aria-labelledby="tableTitle"
+                    aria-label="enhanced table"
+                >
+                    <TableBody>
+                        <TableRow>
+                            {videoItems}
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
+            <TablePagination
+                component="div"
+                count={3}
+                labelRowsPerPage=""
+                rowsPerPageOptions={[]}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                backIconButtonProps={{
+                    'aria-label': 'previous page',
+                }}
+                nextIconButtonProps={{
+                    'aria-label': 'next page',
+                }}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+        </Paper>
+        ) : null
     )
 }
 
